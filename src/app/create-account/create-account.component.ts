ae9886c12eeb1fcf0ddcced1fabde6a2671df859
Carlos Amaral <72256470+carlosabdoamaral@users.noreply.github.com>
2023-08-08 12:14:39 -0300

    Adding on change handler for form

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AccountModel } from '../utils/models';
+import axios from 'axios';
 
 @Component({
   selector: 'app-create-account',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-account.component.scss']
 })
 export class CreateAccountComponent {
+  countryOptions: string[] = [
+    'BR', 'USA', 'ARG', 'FR', 'OTHER'
+  ]
 
+  account: AccountModel = {
+    name: "",
+    email: "",
+    birthdate: new Date(),
+    country: this.countryOptions[0],
+  }
+
+  handleChangeInput(e: any, field: 'name' | 'email' | 'birthdate' | 'country') {
+    e.preventDefault()
+    this.account[field] = e.target.value
+  }
+
+  async submit(e: any) {
+    e.preventDefault()
+
+    // firebase call
+  }
 }
