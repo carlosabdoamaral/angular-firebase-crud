78b109b510cd1406db4ae998255d5723d58592a5
Carlos Amaral <72256470+carlosabdoamaral@users.noreply.github.com>
2023-08-08 11:24:42 -0300

    adding list page

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+
+enum CountryOptionEnum {
+  BR, USA, ARG, OTHER
+}
 
 interface AccountModel {
   name: string
   email: string
   birthdate: Date
-  
+  country: string,
 }
+
+const accountListMock: AccountModel[] = [
+  {
+    name: "Carlos Amaral",
+    email: "carlosabdoamaral@gmail.com",
+    birthdate: new Date(),
+    country: "BR"
+  },
+  {
+    name: "Jeferson Amaral",
+    email: "jca@gmail.com",
+    birthdate: new Date(),
+    country: "USA"
+  },
+]
 
 @Component({
   selector: 'app-list',
@@ -13,5 +33,9 @@ interface AccountModel {
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  accounts: AccountModel[] = accountListMock
 
+  getFormattedBirthdate(d: Date): string {
+    return moment(d).utc(false).format("DD/MM/YYYY")
+  }
 }
